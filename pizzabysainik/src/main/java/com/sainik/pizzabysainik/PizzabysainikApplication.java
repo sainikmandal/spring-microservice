2fac01711eb358b020e4140e49d4fefc621960b7
sainikm <isainikkhan@gmail.com>
2026-05-03 03:28:52 +0530

    Add new project pizzabysainik

---
+++ pizzabysainik/src/main/java/com/sainik/pizzabysainik/PizzabysainikApplication.java
@@ -0,0 +1,13 @@
+package com.sainik.pizzabysainik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzabysainikApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PizzabysainikApplication.class, args);
+    }
+
+}
---
+++ pizzabysainik/src/test/java/com/sainik/pizzabysainik/PizzabysainikApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sainik.pizzabysainik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzabysainikApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
