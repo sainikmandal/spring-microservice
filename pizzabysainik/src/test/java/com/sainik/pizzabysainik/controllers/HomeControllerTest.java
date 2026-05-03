a27cb12f5a26781f12277d1c88503c17409b652a
sainikm <isainikkhan@gmail.com>
2026-05-03 06:45:08 +0530

    Add HomeControllerTest

---
+++ pizzabysainik/src/test/java/com/sainik/pizzabysainik/controllers/HomeControllerTest.java
@@ -0,0 +1,27 @@
+package com.sainik.pizzabysainik.controllers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+public class HomeControllerTest {
+
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        mockMvc = MockMvcBuilders.standaloneSetup(new HomeController()).build();
+    }
+
+    @Test
+    void homeReturnsHomeView() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("home"));
+    }
+}
